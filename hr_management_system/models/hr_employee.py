f6cdae85241a5e666db96c0ac3454a957d7b6695
mos1em <mohammed.alwasefy@gmail.com>
2020-12-13 13:12:07 +0200

    TIMEOFF ar date [SOLVED]

--- hr_management_system/models/hr_employee.py
+++ hr_management_system/models/hr_employee.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
+from dateutil.parser import parse
 
 
 class HRContractInherit(models.Model):
@@ -17,3 +18,15 @@ class HRContractInherit(models.Model):
     emp_job_data = fields.Char(_('Job Data'))
 
 
+
+class HRTimeOffInherit(models.Model):
+    _inherit = 'hr.leave'
+
+    # this function to solve arabic date error in TIMEOFF module
+    # check git error here > https://github.com/odoo/odoo/issues/62053
+    @api.model
+    def get_unusual_days(self, date_from, date_to=None):
+        date_from = parse(date_from).strftime('%Y-%m-%d')
+        if date_to:
+            date_to = parse(date_to).strftime('%Y-%m-%d')
+        return super(HRTimeOffInherit, self).get_unusual_days(date_from, date_to)
